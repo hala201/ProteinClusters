bb8cec6dfd3cfffd8a77a6c36cec7cc7eaa3cb9d
hala201 <halamurad2000@hotmail.com>
2025-06-14 22:27:59 -0400

    model params and metrics setup

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from preprocessor import preprocess_sequences
 from model import load_model
 from inference import run_inference
 from visualizer import visualize_clusters
-
+from sklearn.metrics import adjusted_rand_score
+import json
 
 def main():
     with open("../config.yaml", "r") as f:
@@ -17,9 +18,12 @@ def main():
     processed = preprocess_sequences(data)
     model = load_model(f"../{config["model_path"]}", processed)
     predictions = run_inference(model, processed)
+    ari = adjusted_rand_score(data["cluster_id"], predictions)
 
     print("Inference completed. Results:", predictions)
     visualize_clusters(processed, predictions, data['cluster_id'])
+    with open("../metrics.json", "w") as f:
+        json.dump({"ari": ari}, f, indent=2)
 
 
 if __name__ == "__main__":
